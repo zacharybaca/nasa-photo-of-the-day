e4bde051a2f23842deecf147d6f56c3d501276cc
zacharybaca <baca.zach@icloud.com>
2019-09-12 08:52:18 -0500

    setstate and added mapped data to div

--- src/App.js
+++ src/App.js
@@ -3,22 +3,26 @@ import axios from 'axios';
 import "./App.css";
 
 function App() {
+  const [dailyPicture, setDailyPicture] = useState([])
   useEffect(() => {
     axios
     .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
     .then(response => {
-      console.log(response);
+      const dailyPicture = response.data
+      setDailyPicture(dailyPicture);
+      console.log(dailyPicture);
     })
     .catch(error => {
       console.log(error);
-    }, [])
-  })
+    });
+  }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div>
+        {dailyPicture.map(image => {
+          return <App key={image} />
+        })}
+      </div>
     </div>
   );
 }
