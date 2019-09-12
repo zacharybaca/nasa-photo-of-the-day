1f6f2b778f21f7fa42a13d04d7e2cd5543e42558
zacharybaca <baca.zach@icloud.com>
2019-09-12 08:18:21 -0500

    added useeffect and retrieved data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+    .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.log(error);
+    }, [])
+  })
   return (
     <div className="App">
       <p>
